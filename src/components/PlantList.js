d2c7849df8762b0c0b78d448a62e640bf891e233
*Collins <nyamburycollins@mail.com>
2025-04-22 00:24:59 +0300

    Assignment Completed

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,23 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
-  return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+
+function PlantList({plants, setPlants}){
+  function handleDelete(id) {
+    fetch(`http://localhost:6001/plants/${id}`, {
+      method: "DELETE"
+    })
+    .then(() => {
+      setPlants((prevPlants) => prevPlants.filter((plant) => plant.id !== id));
+    });
+  }
+
+return (
+    <ul className="cards">
+      {plants.map((plant) => (
+        <PlantCard key={plant.id} plant={plant} onDelete={handleDelete} />
+    ))}
+    </ul>
   );
 }
 
